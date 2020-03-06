c9b8d88b32df205b2e5f274b14ebb7037346fe42
jonmarin <jonsonmarin@gmail.com>
2020-03-06 08:32:17 -0500

    Added some complexity into the interface for experimentations.

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
-basic.forever(function () {
-    led.plotBarGraph(
-    pins.analogReadPin(AnalogPin.P0) - 511,
-    512
-    )
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.pause(2000)
+    while (input.buttonIsPressed(Button.AB)) {
+        led.plotBarGraph(
+        pins.analogReadPin(AnalogPin.P0) - 511,
+        512
+        )
+    }
 })
